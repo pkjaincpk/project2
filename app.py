9c27f0a288b42ffca91c34512c636cc0358b7b75
Ubuntu <azureuser@devtestserver.bflse3byou2enmolqx52fruczf.fx.internal.cloudapp.net>
2026-03-25 06:58:17 +0000

    update message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Project 2 Updated 🚀"
+    return "Project 2 Updated Now! 🚀"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
