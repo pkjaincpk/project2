bda7b42c3b9a4ba0e9455790fc325e8565bb8406
Ubuntu <azureuser@devtestserver.bflse3byou2enmolqx52fruczf.fx.internal.cloudapp.net>
2026-03-25 07:08:27 +0000

    UI update

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Project 2 Updated Now! 🚀"
+    return """
+    <h1>🚀 Project 2 Deployed Successfully</h1>
+    <p>CI/CD is working perfectly!</p>
+    """
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
